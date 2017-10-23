69b151b410235636908156279c92bce01af26e35
Ryan <rscharfer@gmail.com>
2017-10-23 07:41:42 +0200

    a list of decks and number of questions of each deck are displayed in the App Component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import getDeckData from './utils/helpers/getDeckData'
 // import { DeckList }  from './components/DeckList.js';
 
@@ -8,13 +8,13 @@ export default class App extends React.Component {
  
   render() {
 
-    const reactDeck = getDeckData('React');
-    const str = "yo"
-    debugger;
+    const allDecks = getDeckData();
+    
+    
     return (
-      <View style={styles.container}>
-        
-        <Text>{reactDeck.title}</Text>
+      <View>
+        <Text> Deck:</Text>
+        {Object.keys(allDecks).map(deck=><Text key={deck}>{allDecks[deck].title}&nbsp;&nbsp;{allDecks[deck].questions.length}</Text>)}
 
       </View>
 
@@ -23,14 +23,7 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
 
 
 

429b45932b2dfff7a55b790cfe6d1566f7477341
Ryan Scharfer <rscharfer@gmail.com>
2017-10-25 13:32:20 +0200

    printing out this.state.deckName

--- components/CreateNewDeck.js
+++ components/CreateNewDeck.js
@@ -27,22 +27,21 @@ export default function CreateNewDeck (props){
 	return (
 		<View>
 			<Text>Create a New Deck</Text>
-			<Text>Name of Deck</Text>
+			<Text>Name of Deck: {this.state.deckName}</Text>
+
 			<TextInput
 				style={{height:40, borderColor:'gray', borderWidth:"1"}}
 				onChangeText={(deckName) => this.setState({deckName})}
 				value={this.state.deckName}
 			/>
 
-
 			<TouchableNativeFeedback
 				background={TouchableNativeFeedback.SelectableBackground()}
 				onpress={handlePress}
 			>
-
-			<View style={{width:150,height:100,backgroundColor:'red'}}>
-				<Text style={{margin:30}}>Create Deck</Text>
-			</View>
+				<View style={{width:150,height:100,backgroundColor:'red'}}>
+					<Text style={{margin:30}}>Create Deck</Text>
+				</View>
 			</TouchableNativeFeedback>
 		</View>
 

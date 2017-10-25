3c45a218c4aed04fe17a3c53fd631c3278ce188d
Ryan <rscharfer@gmail.com>
2017-10-25 14:28:30 +0200

    DeckOverview now has a flatList component

--- components/DeckOverview.js
+++ components/DeckOverview.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, ScrollView, FlatList } from 'react-native'
 import getDeckData from '../utils/helpers/getDeckData'
 import DeckTitleAndQuestionTotal from './DeckTitleAndQuestionTotal'
 import CreateNewDeckButton from './CreateNewDeckButton'
@@ -12,6 +12,14 @@ export default function DeckOverview (props){
 
 	const allDecks = getDeckData();
 
+	const renderItem = ({item}) => {
+
+		// return <DeckTitleAndQuestionTotal {...item} />
+
+		return <DeckTitleAndQuestionTotal key={item.title} title={item.title} questionTotal={item.questions.length} />
+
+	}
+
 
 	return (
 		
@@ -19,10 +27,11 @@ export default function DeckOverview (props){
 
 			<Text>Decks</Text>
 				<View>
-				 {
-					Object.keys(allDecks).map(deck=><DeckTitleAndQuestionTotal key={deck} title={allDecks[deck].title} questionTotal={allDecks[deck].questions.length} />)
-
-				 }
+				<FlatList
+					data={allDecks}
+					renderItem={renderItem}
+				/>
+		
 				 </View>
 			 <CreateNewDeckButton/>
 		</View>

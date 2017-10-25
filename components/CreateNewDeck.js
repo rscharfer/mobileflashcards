51dc36af6561c91cd45fc1022211de0b3fc977e5
Ryan Scharfer <rscharfer@gmail.com>
2017-10-25 13:28:05 +0200

    add react native TextInput component. We are already using a Touch

--- components/CreateNewDeck.js
+++ components/CreateNewDeck.js
@@ -1,32 +1,48 @@
 import React from 'react'
-import { Text, View, TouchableNativeFeedback } from 'react-native'
+import { Text, View, TouchableNativeFeedback, TextInput } from 'react-native'
 
 
 
 
 export default function CreateNewDeck (props){
 
+
+	constructor(props){
+
+
+		this.state={
+			deckName:'Flowers of South Africa',
+
+		}
+	}
+
 	function handlePress(){
 
+		const deckname = this.state.deckName;
+		// add the value to app's decks
+		// 
 
-		alert('You pressed a touchable native feedback component')
 	}
 
 	return (
 		<View>
 			<Text>Create a New Deck</Text>
 			<Text>Name of Deck</Text>
-			<Text>Input Box</Text>
+			<TextInput
+				style={{height:40, borderColor:'gray', borderWidth:"1"}}
+				onChangeText={(deckName) => this.setState({deckName})}
+				value={this.state.deckName}
+			/>
+
+
 			<TouchableNativeFeedback
 				background={TouchableNativeFeedback.SelectableBackground()}
 				onpress={handlePress}
-
 			>
 
 			<View style={{width:150,height:100,backgroundColor:'red'}}>
-				<Text>Create Deck</Text>
+				<Text style={{margin:30}}>Create Deck</Text>
 			</View>
-
 			</TouchableNativeFeedback>
 		</View>
 

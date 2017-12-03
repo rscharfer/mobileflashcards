755942f647ae172ddb24e5588571070f8eb6538d
Ryan <rscharfer@gmail.com>
2017-12-03 13:21:02 +0100

    improved spacing with api.js ... what about trying redux persist?

--- utils/api.js
+++ utils/api.js
@@ -1,44 +1,24 @@
 import { AsyncStorage } from 'react-native'
 
-
-
 const DECKS = "DECKS";
 
-
-
-// returns a promise
-
 export function saveDeckTitle(title){
-
 
 	return AsyncStorage.mergeItem( DECKS , JSON.stringify({
 		[title]:{
 			title,
 			questions:[]
-		}
-		})
-	)
-	
-
+		}	
+	}))
 }
 
 
-
-// returns a promise
-
 export function getDecks(){
-
-
 	return AsyncStorage.getItem(DECKS)
 }
 
 
-
-
-// returns a promise
-
 export function getDeck(id){
-
 
 	return AsyncStorage.getItem(DECKS)
 
@@ -53,27 +33,19 @@ export function getDeck(id){
 }
 
 
-// returns a promis
-
-
 export function addCardToDeck({question,answer,deck}){
-
-
 
 	return AsyncStorage.getItem(DECKS)
 
 	.then((result)=>JSON.parse(result))
 	.then((parsedResult)=>{
-		
-		
 		return parsedResult[deck]
 	})
 	.then((deck)=>{
-		
 		deck.questions.push({question,answer})
-	
 		return deck;
 	})
+
 	.then((updatedDeck)=>{
 		AsyncStorage.mergeItem(DECKS,JSON.stringify({[deck]:updatedDeck}))
 	})
